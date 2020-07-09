a40cf1c8b3c2c7d3e6c7ec98e7081e807a52a527
rommiel <rommieljohnfrancisco@gmail.com>
2020-07-09 23:36:16 +0800

    Updates

---
+++ precache-manifest.2e1a85f5b41c5da366e725b000ec43ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f38467a2770d86c6530f07ad4407f9",
+    "url": "/move-robot/index.html"
+  },
+  {
+    "revision": "21488f9f56ca95316d50",
+    "url": "/move-robot/static/css/main.9dc1fec6.chunk.css"
+  },
+  {
+    "revision": "4cc5c8be27ba18670b37",
+    "url": "/move-robot/static/js/2.f8abe477.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/move-robot/static/js/2.f8abe477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21488f9f56ca95316d50",
+    "url": "/move-robot/static/js/main.4396fc7f.chunk.js"
+  },
+  {
+    "revision": "c0b1868f601584ae97a5",
+    "url": "/move-robot/static/js/runtime-main.3b63032d.js"
+  }
+]);
